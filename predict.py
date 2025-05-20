e198460ad71012b543837461c8038510cb336f98
Zikaal <168971723+Zikaal@users.noreply.github.com>
2025-05-20 23:31:35 +0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,28 +4,23 @@ import json
 import joblib
 
 def main():
-    # Загрузка модели
     model = joblib.load("news_ensemble_model.pkl")
 
-    # Получаем вход из аргумента
     if len(sys.argv) < 2:
         print(json.dumps({"error": "No input provided"}))
         sys.exit(1)
     text = sys.argv[1]
 
-    # Предсказание
     proba = model.predict_proba([text])[0]
     classes = model.classes_.tolist()
     probabilities = [round(p * 100, 2) for p in proba]
     prediction = classes[probabilities.index(max(probabilities))]
 
-    # Выводим JSON
-    result = {
+    print(json.dumps({
         "classes": classes,
         "probabilities": probabilities,
         "prediction": prediction
-    }
-    print(json.dumps(result, ensure_ascii=False))
+    }, ensure_ascii=False))
 
 if __name__ == "__main__":
     main()
