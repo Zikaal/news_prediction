afce958975059a34a952dadec1b7bc7703c0c161
Zikaal <168971723+Zikaal@users.noreply.github.com>
2025-05-20 23:16:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'templates')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'templates', 'index.html'));
